5a781c52257c1f339aac73fc1f151dcd2b8e0243
tenessy0570 <tenessy0570@gmail.com>
2024-07-20 22:07:14 +0700

    updated db connection

--- db/connection.py
+++ db/connection.py
@@ -1,5 +1,9 @@
 from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import AsyncSession, async_sessionmaker, create_async_engine
 
 from config import PATH_TO_DB_FILE
 
 engine = create_engine(fr"sqlite:///{PATH_TO_DB_FILE.absolute()}", echo=False)
+async_engine = create_async_engine(fr"sqlite+aiosqlite:///{PATH_TO_DB_FILE.absolute()}", echo=False)
+
+async_db_session = async_sessionmaker(async_engine)()
