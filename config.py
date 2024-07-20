8c56c62933abd639ebe28f5867bc5fcd70c140df
tenessy0570 <tenessy0570@gmail.com>
2024-07-20 22:34:23 +0700

    updated config with logger instnace

--- config.py
+++ config.py
@@ -1,4 +1,10 @@
+import logging
 from pathlib import Path
+
+format_ = "[%(name)s][%(levelname)s][%(asctime)s] on line %(lineno)d - %(filename)s.%(funcName)s - %(message)s"
+logging.basicConfig(format=format_, level=logging.INFO)
+
+logger = logging.getLogger("app")
 
 ROOT_PATH = Path(__file__).parent
 PATH_TO_DB_FILE = ROOT_PATH.absolute() / "db" / "db.db"
